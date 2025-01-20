f0e522c942d5b3e960d4160d9b47936cb7acfa52
Hyunwc <107088369+Hyunwc@users.noreply.github.com>
2025-01-20 12:37:03 +0900

    01_20_HUD
    HUD 작업

--- Source/ArenaBattle/Player/ABPlayerController.cpp
+++ Source/ArenaBattle/Player/ABPlayerController.cpp
@@ -2,6 +2,18 @@
 
 
 #include "Player/ABPlayerController.h"
+#include "UI/ABHUDWidget.h"
+
+AABPlayerController::AABPlayerController()
+{
+	static ConstructorHelpers::FClassFinder<UABHUDWidget> ABHUDWidgetRef(TEXT("/Game/ArenaBattle/UI/WBP_ABHUD.WBP_ABHUD_C"));
+	if (ABHUDWidgetRef.Class)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ABHUDWidget class found successfully."));
+		//UE_LOG(LogTemp, Warning, TEXT("good"));
+		ABHUDWidgetClass = ABHUDWidgetRef.Class;
+	}
+}
 
 void AABPlayerController::BeginPlay()
 {
@@ -10,4 +22,10 @@ void AABPlayerController::BeginPlay()
 	// 시작하자마자 마우스가 뷰포트 안으로 들어가게 된다.
 	FInputModeGameOnly GameOnlyInputMode;
 	SetInputMode(GameOnlyInputMode);
+
+	ABHUDWidget = CreateWidget<UABHUDWidget>(this, ABHUDWidgetClass);
+	if (ABHUDWidget)
+	{
+		ABHUDWidget->AddToViewport();
+	}
 }
